c31c495baff08b2ecab3507bed124b5723f4a1f6
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-03-31 19:39:54 +0100

    Converts 2 into 10

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,5 +1,27 @@
 class Converter 
+    attr_reader :results
+
+    def initialize
+        @results = []
+    end
+
     def convert(ip_address)
-        ip_address
+        decimals = ip_address.split('')
+        to_binary(decimals)
+        display
+    end
+
+    def to_binary(decimals)
+        decimals.map do |decimal|
+            if decimal == "2"
+                @results.push("10")
+            else 
+                @results.push(decimal)
+            end
+        end
+    end
+
+    def display
+        @results.join
     end
 end
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -2,9 +2,14 @@ require 'converter'
 
 describe Converter do
     describe '#convert' do
-        it 'takes a string as an input' do
-            ip_address = "00.000.00.00"
-            expect(subject.convert(ip_address)).to eq "00.000.00.00"
+        it 'does not convert 0 and 1' do
+            ip_address = "01.010.01.01"
+            expect(subject.convert(ip_address)).to eq "01.010.01.01"
+        end
+
+        it 'converts 2 into 10' do
+            ip_address = "02.020.01.01"
+            expect(subject.convert(ip_address)).to eq "010.0100.01.01"
         end
     end
 end
